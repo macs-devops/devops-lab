3ba925ea2f712e3150384606e4274c263a6b301f
Max Florentin <86262167+maxflorentin@users.noreply.github.com>
2022-07-28 19:41:30 -0300

    Create provider.tf

---
+++ aws/terraform/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.23.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                  = var.region
+  profile                 = var.profile
+  shared_credentials_file = "/Users/maximiliano.florentin/.aws/credentials"
+}
