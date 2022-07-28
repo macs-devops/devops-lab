35b883740cfe33f69e5f2ff129052fe975a2ecf6
Max Florentin <maximiliano.florentin@ank.app>
2022-07-28 19:49:43 -0300

    upload tf files

---
+++ aws/terraform/backend.tf
@@ -0,0 +1,16 @@
+module "backend" {
+  source         = "github.com/maxflorentin/terraform-aws-backend"
+  backend_bucket = "max-devops-tfstate"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "max-devops-tfstate"
+    key            = "states/terraform.tfstate"
+    dynamodb_table = "tf-lock"
+    encrypt        = true
+    region         = "us-east-1"
+    profile        = "max-devops-adm"
+  }
+}
+
---
+++ aws/terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "s3_backend_bucket_name" {
+  value = module.backend.s3_backend_bucket_name
+}
+
+output "dynamodb_lock_table_name" {
+  value = module.backend.dynamodb_lock_table_name
+}
+
+output "dynamodb_lock_table_arn" {
+  value = module.backend.dynamodb_lock_table_arn
+}
+
+output "dynamodb_lock_stream_arn" {
+  value = module.backend.dynamodb_lock_stream_arn
+}
+
+output "dynamodb_lock_stream_label" {
+  value = module.backend.dynamodb_lock_stream_label
+}
