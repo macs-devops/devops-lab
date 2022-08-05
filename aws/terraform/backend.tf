bddf8c420c2d9399a842ac917b92f0bbc603f2ab
Max Florentin <maximiliano.florentin@ank.app>
2022-08-05 12:06:57 -0300

    rename dynamodb table

--- aws/terraform/backend.tf
+++ aws/terraform/backend.tf
@@ -7,7 +7,7 @@ terraform {
   backend "s3" {
     bucket         = "max-devops-tfstate"
     key            = "states/terraform.tfstate"
-    dynamodb_table = "tf-lock"
+    dynamodb_table = "terraform-lock"
     encrypt        = true
     region         = "us-east-1"
     profile        = "max-devops-adm"
