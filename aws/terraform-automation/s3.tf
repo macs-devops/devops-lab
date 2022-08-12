bf4a51c3ee35fcbdee8ae7bcc80cd847e46df611
Max Florentin <maximiliano.florentin@ank.app>
2022-08-12 07:56:26 -0300

    s3 add kms

--- aws/terraform-automation/s3.tf
+++ aws/terraform-automation/s3.tf
@@ -1,7 +1,6 @@
-resource "aws_s3_bucket" "onebucket" {
+resource "aws_s3_bucket" "gh-bucket" {
   bucket = "gh-actions-automation"
   acl    = "private"
-  region = "us-east-1"
 
   versioning {
     enabled = true
@@ -9,7 +8,27 @@ resource "aws_s3_bucket" "onebucket" {
 
   tags = {
     Name  = "gh-actions"
-    Owner = "Max"
+    Owner = "max-devops"
   }
 }
 
+logging {
+  target_bucket = aws_s3_bucket.gh-bucket.id
+  target_prefix = "log/"
+}
+
+resource "aws_kms_key" "gh-kms" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "gh-bucket-kms" {
+  bucket = aws_s3_bucket.gh-bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.gh-kms.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
