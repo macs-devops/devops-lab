3737da4067ced49a1c1a782a7cfe568e044b6b95
Max Florentin <maximiliano.florentin@ank.app>
2022-08-12 08:07:16 -0300

    s3 fix logging block

--- aws/terraform-automation/s3.tf
+++ aws/terraform-automation/s3.tf
@@ -11,6 +11,14 @@ resource "aws_s3_bucket" "gh-bucket" {
     target_prefix = "log/"
   }
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.gh-kms.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+
   tags = {
     Name  = "gh-actions"
     Owner = "max-devops"
@@ -20,15 +28,4 @@ resource "aws_s3_bucket" "gh-bucket" {
 resource "aws_kms_key" "gh-kms" {
   description             = "This key is used to encrypt bucket objects"
   deletion_window_in_days = 10
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "gh-bucket-kms" {
-  bucket = aws_s3_bucket.gh-bucket.bucket
-
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.gh-kms.arn
-      sse_algorithm     = "aws:kms"
-    }
-  }
 }
