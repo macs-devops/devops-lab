f10e6f0555855c56ebd95b16921aa85eb01a479c
Max Florentin <maximiliano.florentin@ank.app>
2022-08-13 21:20:30 -0300

    add version provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,7 @@
 # terraform provider aws with shared_credentials_file
 provider "aws" {
-  version                 = "~> 2.0"
-  shared_credentials_file = "~/.aws/credentials"
-  profile                 = "terraform"
+  version                 = "~> 4.0"
   region                  = "us-east-1"
+  shared_credentials_file = "~/.aws/credentials"
+  profile                 = "max-devops-adm"
 }
