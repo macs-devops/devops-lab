ce3328f12a32f875be7313eba38353f9e07ceb7f
Max Florentin <maximiliano.florentin@ank.app>
2022-08-15 20:23:47 -0300

    add fmt to pipeline

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,3 +18,15 @@ variable "dynamodb_policy_name" {
   type        = string
   description = "Policy permission dynamodb"
 }
+
+variable "WS_ACCESS_KEY_ID" {
+  type        = string
+  description = "github secret WS_ACCESS_KEY_ID"
+  sensitive   = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type        = string
+  description = "github secret AWS_SECRET_ACCESS_KEY"
+  sensitive   = true
+}
