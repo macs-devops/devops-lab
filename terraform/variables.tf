25995d15d963dc38da33e332c8e2ff32b70d8f34
Max Florentin <maximiliano.florentin@ank.app>
2022-08-15 20:30:09 -0300

    fix variable aws

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -22,11 +22,9 @@ variable "dynamodb_policy_name" {
 variable "AWS_ACCESS_KEY_ID" {
   type        = string
   description = "github secret AWS_ACCESS_KEY_ID"
-  sensitive   = true
 }
 
 variable "AWS_SECRET_ACCESS_KEY" {
   type        = string
   description = "github secret AWS_SECRET_ACCESS_KEY"
-  sensitive   = true
 }
