a992952010819da16d4c3f307b7238254ab38f3e
Max Florentin <maximiliano.florentin@ank.app>
2022-08-15 21:22:57 -0300

    add output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "openid_connect_provider" {
   description = "AWS OpenID Connected identity provider."
-  value       = aws_iam_openid_connect_provider.github_actions
+  value       = aws_iam_openid_connect_provider.github
 }
 
 
