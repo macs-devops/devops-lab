d6c08d23271ff8b1261e31457537fe8410ba672f
Max Florentin <maximiliano.florentin@ank.app>
2022-08-16 10:23:49 -0300

    test bucket encryption

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -3,17 +3,21 @@ resource "aws_s3_bucket" "bucket" {
   versioning {
     enabled = true
   }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
+
   object_lock_configuration {
     object_lock_enabled = "Enabled"
   }
   tags = {
     Name = "S3 Remote Terraform State Store"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
