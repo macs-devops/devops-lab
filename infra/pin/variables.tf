c6ffcbf1c7f196b68f78036a757d3e4660687d91
Max Florentin <maximiliano.florentin@ank.app>
2022-08-17 18:53:49 -0300

    edit vpc pin

--- infra/pin/variables.tf
+++ infra/pin/variables.tf
@@ -1,36 +1,42 @@
-variable "region" {
-     default = "us-east-1"
-}
 variable "availabilityZone" {
-     default = "us-east-1a"
+  default = "us-east-1a"
 }
+
 variable "instanceTenancy" {
-    default = "default"
+  default = "default"
 }
+
 variable "dnsSupport" {
-    default = true
+  default = true
 }
+
 variable "dnsHostNames" {
-    default = true
+  default = true
 }
+
 variable "vpcCIDRblock" {
-    default = "10.0.0.0/16"
+  default = "10.0.0.0/16"
 }
+
 variable "subnetCIDRblock" {
-    default = "10.0.1.0/24"
+  default = "10.0.1.0/24"
 }
+
 variable "destinationCIDRblock" {
-    default = "0.0.0.0/0"
+  default = "0.0.0.0/0"
 }
+
 variable "ingressCIDRblock" {
-    type = list
-    default = [ "0.0.0.0/0" ]
+  type    = list(any)
+  default = ["0.0.0.0/0"]
 }
+
 variable "egressCIDRblock" {
-    type = list
-    default = [ "0.0.0.0/0" ]
+  type    = list(any)
+  default = ["0.0.0.0/0"]
 }
+
 variable "mapPublicIP" {
-    default = true
+  default = true
 }
 
