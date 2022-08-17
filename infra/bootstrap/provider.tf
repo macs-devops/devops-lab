f897fa8a1f413043c95f6104e9328b3997e4e5c6
Max Florentin <maximiliano.florentin@ank.app>
2022-08-17 15:29:58 -0300

    another try

--- infra/bootstrap/provider.tf
+++ infra/bootstrap/provider.tf
@@ -8,6 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region  = var.aws_region
-  profile = "max-devops-adm"
+  region                      = var.aws_region
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
 }
