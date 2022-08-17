a3ef69d20008a78836bb2dc0c795c7599fe6ab89
Max Florentin <maximiliano.florentin@ank.app>
2022-08-17 15:44:00 -0300

    fix main module

--- infra/gh-oidc/main.tf
+++ infra/gh-oidc/main.tf
@@ -1,7 +1,5 @@
 locals {
-  role_name        = "${var.namespace}-github-action"
-  main_policy_arns = [aws_iam_policy.main.arn]
-  policy_arns      = var.bootstrap_enabled ? concat(local.main_policy_arns, [aws_iam_policy.bootstrap.arn]) : local.main_policy_arns
+  role_name = "${var.namespace}-github-action"
 }
 
 module "iam_role" {
@@ -10,7 +8,7 @@ module "iam_role" {
 
   attach_read_only_policy = false
   iam_role_name           = local.role_name
-  iam_role_policy_arns    = local.policy_arns
+  iam_role_policy_arns    = ["arn:aws:iam::593043004533:policy/terraform-github-action-main", "arn:aws:iam::593043004533:policy/terraform-github-action-bootstrap"]
   github_repositories     = ["${var.github_org}/${var.github_repo}"]
 }
 
