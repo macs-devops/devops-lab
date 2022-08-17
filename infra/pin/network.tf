8c67a36843d604c24255def8a69c27fe0ecb03a5
Max Florentin <maximiliano.florentin@ank.app>
2022-08-17 18:38:12 -0300

    edit vpc resource

--- infra/pin/network.tf
+++ infra/pin/network.tf
@@ -1,8 +1,27 @@
-resource "aws_vpc" "pin" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  tags {
-    Name = "pin"
+data "aws_region" "current" {}
+
+resource "aws_vpc_ipam" "pin" {
+  operating_regions {
+    region_name = data.aws_region.current.name
   }
 }
+
+resource "aws_vpc_ipam_pool" "pin" {
+  address_family = "ipv4"
+  ipam_scope_id  = aws_vpc_ipam.pin.private_default_scope_id
+  locale         = data.aws_region.current.name
+}
+
+resource "aws_vpc_ipam_pool_cidr" "pin" {
+  ipam_pool_id = aws_vpc_ipam_pool.pin.id
+  cidr         = "172.2.0.0/16"
+}
+
+resource "aws_vpc" "pin" {
+  ipv4_ipam_pool_id   = aws_vpc_ipam_pool.pin.id
+  ipv4_netmask_length = 28
+  depends_on = [
+    aws_vpc_ipam_pool_cidr.pin
+  ]
+}
+
