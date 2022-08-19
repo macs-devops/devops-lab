d5a5cacde1d6566dd72ab7aed220a1c939fb20af
Max Florentin <maximiliano.florentin@ank.app>
2022-08-19 09:15:01 -0300

    change module

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -1,18 +1,11 @@
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
+resource "aws_instance" "pin2022" {
 
-  name = "single-instance"
-
-  ami                    = "ami-052efd3df9dad4825"
-  instance_type          = "t2.micro"
-  key_name               = "pin2022"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-0fd5214d272e690d6"]
-  subnet_id              = "subnet-00874fe6cd236e4b2"
+  ami           = "ami-052efd3df9dad4825"
+  instance_type = "t2.micro"
 
   tags = {
     Terraform   = "true"
     Environment = "terraform"
   }
 }
+
