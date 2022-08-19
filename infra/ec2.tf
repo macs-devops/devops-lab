eda9632310f22880dd625f8099ab777733b37f06
Max Florentin <maximiliano.florentin@ank.app>
2022-08-19 08:41:37 -0300

    test module ec2

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -2,11 +2,7 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
 
-  name = "spot-instance"
-
-  create_spot_instance = true
-  spot_price           = "0.60"
-  spot_type            = "persistent"
+  name = "single-instance"
 
   ami                    = "ami-ebd02392"
   instance_type          = "t2.micro"
