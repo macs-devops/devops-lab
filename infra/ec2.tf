3f04b8c8778b7676e06487c7670deb0b50695ab2
Max Florentin <maximiliano.florentin@ank.app>
2022-08-19 08:39:11 -0300

    test module ec2

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -1,8 +1,22 @@
-resource "aws_instance" "web" {
-  ami           = "ami-08ae71fd7f1449df1"
-  instance_type = "t2.micro"
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "spot-instance"
+
+  create_spot_instance = true
+  spot_price           = "0.60"
+  spot_type            = "persistent"
+
+  ami                    = "ami-ebd02392"
+  instance_type          = "t2.micro"
+  key_name               = "pin-2022"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-08f8123a6bcb15ef2"]
+  subnet_id              = "subnet-0382a2bd272f58b0a"
 
   tags = {
-    Name = "ec2-pin2022"
+    Terraform   = "true"
+    Environment = "terraform"
   }
 }
