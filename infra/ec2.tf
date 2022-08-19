44875381e6dd50e577d74339c179be06508df1a1
Max Florentin <maximiliano.florentin@ank.app>
2022-08-19 08:44:25 -0300

    test module ec2

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -4,7 +4,7 @@ module "ec2_instance" {
 
   name = "single-instance"
 
-  ami                    = "ami-ebd02392"
+  ami                    = "ami-08ae71fd7f1449df1"
   instance_type          = "t2.micro"
   key_name               = "pin-2022"
   monitoring             = true
