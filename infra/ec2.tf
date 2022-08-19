504386480d65dcbc06e026263f1fc86a19be80d7
Max Florentin <maximiliano.florentin@ank.app>
2022-08-19 14:55:44 -0300

    ec2 add userdata script

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -3,6 +3,8 @@ resource "aws_instance" "pin2022" {
   ami           = "ami-052efd3df9dad4825"
   instance_type = "t2.micro"
 
+  user_data = file("userdata.sh")
+
   tags = {
     Terraform   = "true"
     Environment = "terraform"
