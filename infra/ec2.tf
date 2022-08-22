02fc0c0b1592f7798a02a320a17629ed56fe2bc4
Max Florentin <maximiliano.florentin@ank.app>
2022-08-22 08:03:12 -0300

    ec2 add key

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "pin2022" {
 
   ami           = "ami-052efd3df9dad4825"
   instance_type = "t2.micro"
-
-  user_data = file("userdata.sh")
+  user_data     = file("userdata.sh")
+  key_name      = "pin2022"
 
   vpc_security_group_ids = [aws_security_group.pin.id]
 
