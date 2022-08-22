61d67a60aa3afd254f573d4e6f7eac635e9236a0
Max Florentin <maximiliano.florentin@ank.app>
2022-08-22 15:55:08 -0300

    policy eks

--- infra/policies.tf
+++ infra/policies.tf
@@ -20,3 +20,8 @@ resource "aws_iam_policy" "pin2022-ec2-policy" {
 }
 EOF
 }
+
+resource "aws_iam_role_policy_attachment" "github-eks-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = "terraform-github-action"
+}
